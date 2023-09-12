8a6fdc21d8d61fc88a362e9ccb88eb276cce1c2f
Amnok <ankitsingh80741@gmail.com>
2023-09-12 18:17:12 +0530

    added custom button

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Pressable } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
+import AppButton from './app/components/AppButton';
 
 export default function App() {
   return (
-    <ViewImageScreen/>
+    <View style={{marginTop: 40}}>
+      <AppButton title='login' onPress={() => console.log('here')}/>
+    </View>
   );
 }
