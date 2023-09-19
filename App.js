c8348f2fc04187e6ed1cd9db0730b5f058c90085
Amnok <ankitsingh80741@gmail.com>
2023-09-19 14:53:35 +0530

    reusable icon added

--- App.js
+++ App.js
@@ -5,9 +5,17 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/components/MessageScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 export default function App() {
-  return (
-<MessagesScreen/>
-  );
+  return <Screen>
+<Icon
+ name="email"
+ size={50}
+ backgroundColor="red"
+ iconColor="white"
+/>
+
+  </Screen>;
 }
